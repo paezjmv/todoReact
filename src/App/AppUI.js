53504bfde12f994fa681486584cb95634aedc534
Martin Paez <martinpaez@MacBook-Air-de-Martin.local>
2025-02-08 17:40:55 -0600

    [ADD] Loading skeletons

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -3,6 +3,9 @@ import { TodoCounter } from '../TodoCounter/';
 import { TodoSearch } from '../TodoSearch/'
 import { TodoList } from '../TodoList/'
 import { TodoItem } from '../TodoItem/'
+import { TodosLoading } from '../TodosLoading/'
+import { TodosError } from '../TodosError/'
+import { EmptyTodos } from  '../EmptyTodos/'
 import { CreateTodoButton } from '../CreateTodoButton/'
 
 function AppUI ({
@@ -30,9 +33,15 @@ function AppUI ({
       />
 
       <TodoList>
-        {loading && <p>Estamos Cargando...</p>}
-        {error && <p>Desesperate, hubo un error!!...</p>}
-        {(!loading && searchedTodos.length === 0) && <p>Crea tu primer TODO!</p>}
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
 
         {searchedTodos.map(todo => (
           <TodoItem 
